ac5233e52b86cee9f324664ec815b7b8e09886c2
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-03-20 20:33:58 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int sum,a,b;
+   cout<<"Enter the value of a";
+   cin>>a;
+   cout<<"Enter the value of b";
+   cin>>b;
+   sum=a+b;
+   cout<<a<<"+"<<b<<"="<<sum;
+    return 0;
+}

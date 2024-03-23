40c5b3d4aa16a29b54dd671c507e77e8a85a031e
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-03-23 20:04:53 +0530

    Add files via upload

---
+++ sub.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int a,b,sub;
+  cout<<"Enter the value of a"<<endl;
+  cin>>a;
+  cout<<"Enter the value of b"<<endl;
+  cin>>b;
+  sub=a-b;
+  cout<<a<<"-"<<b<<"="<<sub;
+    return 0;
+}

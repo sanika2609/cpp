f494b495eafd920d826ffbfab5269d641724688f
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-03-24 20:53:40 +0530

    Add files via upload

---
+++ even_odd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int even,odd,num;
+   cout<<"Enter the number"<<endl;
+   cin>>num;
+   
+   if(num%2==0)
+   {
+       cout<<"The number is Even"<<endl;
+   }
+   else
+   {
+       cout<<"The number is odd"<<endl;
+   }
+    return 0;
+}

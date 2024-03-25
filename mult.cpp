c1627c8c35c2098b062eb0ddab597c9f9951b17f
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-03-25 20:16:02 +0530

    Add files via upload

---
+++ mult.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int a,b,mult;
+   cout<<"Enter the value of a"<<endl;
+   cin>>a;
+   cout<<"Enter the value of b"<<endl;
+   cin>>b;
+   mult=a*b;
+   cout<<a<<"*"<<b<<"="<<a*b;
+    return 0;
+}

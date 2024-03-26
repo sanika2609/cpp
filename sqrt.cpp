94a0f94ebe0138a8ac63fdc70d37d066484e635f
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-03-26 20:26:52 +0530

    Add files via upload

---
+++ sqrt.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main() {
+   int a;
+   cout<<"Enter the value of a"<<endl;
+   cin>>a;
+   int root=sqrt(a);
+   cout<<root<<endl;
+
+    return 0;
+}

9eb0f71842aff75038f6aabed1ff6cc3ca211771
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-03-27 19:10:33 +0530

    Add files via upload

---
+++ age.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int age;
+  cout<<"Enter the age of person"<<endl;
+  cin>>age;
+  if(age>=18)
+  {
+      cout<<"The person can eligible for vote"<<endl;
+  }
+  else
+  {
+      cout<<"the person can't eligible for vote"<<endl;
+  }
+    return 0;
+}

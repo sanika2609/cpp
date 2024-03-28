8a6babe51dc51f5c4ce92842de9d79e8c419f8af
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-03-28 22:40:31 +0530

    Add files via upload

---
+++ greater.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cout<<"Enter the vlue of a"<<endl;
+    cin>>a;
+    cout<<"Enter the value of b"<<endl;
+    cin>>b;
+    if(a>b)
+    {
+        cout<<"a is the greatest number"<<endl;
+    }
+    else
+    {
+        cout<<"b is the greatest number"<<endl;
+    }
+
+    return 0;
+}

2452ddb76635e429eea5c5bb5743e1c977b8bfc6
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-03-29 20:36:08 +0530

    Add files via upload

---
+++ smallest.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int a=3;
+   int b=4;
+   if(a<b)
+   {
+       cout<<"The smallest number is a"<<endl;
+   }
+   else
+   {
+        cout<<"The smallest number is b"<<endl;
+   }
+
+    return 0;
+}

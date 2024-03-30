e96edb8bf65c3cc52cc415a9c18ea08736d3f088
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-03-30 21:59:58 +0530

    Add files via upload

---
+++ table.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the n"<<endl;
+    cin>>n;
+for(int i=1;i<=10;i++)
+     
+             cout<<i*n<<endl;;
+    return 0;
+}

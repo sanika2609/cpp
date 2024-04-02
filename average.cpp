5ee3dab5f909d7a0f788f142b75b306864028193
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-04-02 19:54:19 +0530

    Add files via upload

---
+++ average.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,avg,sum;
+    cout<<"Enter the a"<<endl;;
+    cin>>a;
+     cout<<"Enter the b"<<endl;
+    cin>>b;
+     cout<<"Enter the c"<<endl;
+    cin>>c;
+    sum=a+b+c;
+
+    avg=sum/3;
+    cout<<avg<<endl;
+    return 0;
+}

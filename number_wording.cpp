5e55206cf33cd5aebd3ede941efd0e7629276fa5
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-04-05 20:52:38 +0530

    Add files via upload

---
+++ number_wording.cpp
@@ -0,0 +1,54 @@
+// Online C++ compiler to run C++ program online
+
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cout<<"Enter n";
+    cin>>n;
+    if(n==1)
+    {
+        cout<<"one"<<endl;
+    }
+    else if(n==2)
+    {
+        cout<<"two"<<endl;
+    }
+    else if(n==3)
+    {
+        cout<<"three"<<endl;
+    }
+    else if(n==4)
+    {
+        cout<<"four"<<endl;
+    }
+    else if(n==5)
+    {
+        cout<<"five"<<endl;
+    }
+    else if(n==6)
+    {
+        cout<<"six"<<endl;
+    }    
+    else if(n==7)
+    {
+        cout<<"seven"<<endl;
+    }
+    else if(n==8)
+    {
+        cout<<"eight"<<endl;
+    }
+    else if(n==9)
+    {
+        cout<<"nine"<<endl;
+    }
+    else 
+    {
+        cout<<"Number is greater then Nine";
+    }
+    
+    return 0;
+}

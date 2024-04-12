27f45fb877c02ba8d38197337d57b7f57b364845
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-04-12 19:55:40 +0530

    Add files via upload

---
+++ arr_sum.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int i,arr[]={1,2,3,4,5,6,7,8,9,10};
+    int sum=arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5]+arr[6]+arr[7]+arr[8]+arr[9];
+    cout<<"The sum of all array elements is"<<sum;
+    return 0;
+}

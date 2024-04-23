65a8bec29455f778d4cb7af6b45ca8a7d8a45be0
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-04-23 20:43:00 +0530

    Add files via upload

---
+++ simple_interest.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+   float p=10000;
+   float r=30;
+   float t=3;
+   float si=(p*r*t)/100;
+   cout<<"The simple interest of given amount is:"<<si;
+    return 0;
+}

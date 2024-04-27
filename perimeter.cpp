a9e0455ea9a58a3239836c410620e736ff857519
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-04-27 22:15:56 +0530

    Add files via upload

---
+++ perimeter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+ int l,b;
+ int perimeter;
+ cout<<"Enter the length of rectangle"<<endl;
+ cin>>l;
+  cout<<"Enter the breadth of rectangle"<<endl;
+ cin>>b;
+ perimeter=2*(l+b);
+ cout<<"perimeter of the rectangle is:"<<perimeter;
+    return 0;
+}

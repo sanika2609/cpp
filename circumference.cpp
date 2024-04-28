641c946635dd56136c970d26b3e001c5e5afff46
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-04-28 20:57:13 +0530

    Add files via upload

---
+++ circumference.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+   float circumference;
+   float pie=3.14;
+   int r;
+   cout<<"Enter the radius of circle";
+   cin>>r;
+   circumference=2*pie*r;
+   cout<<"circumference of the circle is:"<<circumference;
+    return 0;
+}

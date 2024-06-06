37aedb72ce2f74af48a9f87d2ab13866ea5c9722
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-06-06 22:17:00 +0530

    Create Diet.cpp

---
+++ Diet.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char diet;
+cout<<"Enter the diet";
+cin<<diet;
+if( diet=='H')
+{
+cout<<"Good";
+}
+else 
+{
+cout<<"bad";
+}
+return 0;
+}

96e2ed9898d418b833d3adb4cc295ea1ca322a27
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-06-08 19:59:17 +0530

    Create Table.cpp

---
+++ Table.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void printTable(n)
+int main()
+{int n;
+cout<<"Enter n";
+cin>>n;
+printTable(n);
+return 0;
+}
+void printTable(int n)
+{
+for(int i=1;i<=10;i++)
+{
+cout<<i*n;
+}

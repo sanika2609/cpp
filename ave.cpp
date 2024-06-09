70a90d847b10299f0df1341c17af8283dfcccabe
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-06-09 22:12:47 +0530

    Create ave.cpp

---
+++ ave.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,avg,sum;
+    cout<<"Enter the a";
+    cin>>a;
+     cout<<"Enter the b";
+    cin>>b;
+     cout<<"Enter the c";
+    cin>>c;
+    sum=a+b+c;
+    cout<<"sum of the three number is"<<sum;
+    avg=sum/3;
+    cout<<"the average of three numbers is"<<avg;
+    return 0;
+}

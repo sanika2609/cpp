751253046d802037f7c0ae0f9ae1c3a193efe9e9
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-06-23 20:37:00 +0530

    Create Ytc.cpp

---
+++ Ytc.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<list>
+using namespace std;
+class Youtubechanel{
+ public: string name;
+  	int subscribers;
+  
+  public: Youtubechanel(string nm,int sub){
+	name=nm;
+	subscribers=sub;
+   
+        }
+//public: void displayInfo(){
+    // cout<<name<<endl;
+    // cout<<subscribers<<endl;
+//}
+};
+class MyCollection{
+ public: list<Youtubechanel>mylist;
+  public: void operator+=(Youtubechanel& chanel){
+  mylist.push_back(chanel);
+}
+
+};
+ostream& operator<<(ostream& COUT,Youtubechanel& ytc)
+{
+ 	COUT<<ytc.name;
+ 	COUT<<ytc.subscribers;
+ 	return COUT;
+}
+ostream& operator<<(ostream& COUT,MyCollection& mc)
+{
+   for(Youtubechanel ytc:mc.mylist)
+{  
+   COUT<<ytc;
+}
+return COUT;
+}
+int main()
+{
+  Youtubechanel Yt1("DKTE",5);
+  Youtubechanel Yt2("SANIKA",1000);
+  //Yt1.displayInfo();
+  //Yt2.displayInfo();
+  MyCollection mc1;
+  mc1+=Yt1;
+  mc1+=Yt2;
+ cout<<mc1;
+  return 0;
+}

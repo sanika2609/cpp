67246c38f422ded360b0924818a814cf100eec82
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-07-04 22:27:24 +0530

    Create Absolute_value.cpp

---
+++ Absolute_value.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int n;
+  cout<<"Enter n";
+  cin>>n;
+  if(n>=0)
+  {
+      cout<<n;
+  }
+  else
+  {
+      cout<<-n;
+  }
+    return 0;
+}

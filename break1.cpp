eea2fa706b2e65bb3ba05fe5f73baecb99450397
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-12-10 15:33:53 +0530

    Create break1.cpp

---
+++ break1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int i=1;
+    while(i<=10)
+    {
+       
+        if(i==5)
+        break;
+        i++;
+         cout<<"\n"<<i;
+    }
+
+    return 0;
+}

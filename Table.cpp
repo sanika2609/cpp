f66b80ee343d8391e346d42025ff01c2a0c38975
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-12-12 15:23:42 +0530

    Add files via upload

--- Table.cpp
+++ Table.cpp
@@ -1,16 +1,16 @@
 #include<iostream>
 using namespace std;
-void printTable(n)
 int main()
-{int n;
-cout<<"Enter n";
-cin>>n;
-printTable(n);
-return 0;
-}
-void printTable(int n)
 {
-for(int i=1;i<=10;i++)
-{
-cout<<i*n;
+    int n;
+    cout<<"Enter number";
+    cin>>n;
+   int i=1;
+    while(i<=10)
+    {
+        cout<<"\n"<<n*i;
+        i++;
+    }
+   
+    return 0;
 }

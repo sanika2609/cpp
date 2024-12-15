c10197906051dad96322de52373a6f07aab478f6
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2024-12-15 22:45:11 +0530

    Add files via upload

---
+++ pyramid1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int i,j,n;
+    cout<<"Enter number of rows";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        cout<<"*";
+        cout<<"\n";
+    }
+    return 0;
+}#include <iostream>
+using namespace std;
+int main() {
+    int i,j,n;
+    cout<<"Enter number of rows";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        cout<<i+j;
+        cout<<"\n";
+    }
+    return 0;
+}

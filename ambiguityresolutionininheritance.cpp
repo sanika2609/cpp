90efaeba30b45cba36c6d9018c4537385ffc0d79
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2025-01-05 23:12:04 +0530

    Add files via upload

---
+++ ambiguityresolutionininheritance.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+class A{
+    protected:
+    int a;
+    public:
+    void input(){
+        cout<<"Enter value of a";
+        cin>>a;
+    }
+    void display(){
+        cout<<"\n a="<<a;
+    }
+};
+class B{
+    protected:
+    int b;
+    public:
+    void input(){
+        cout<<"Enter value of b";
+        cin>>b;
+    }
+    void print(){
+        cout<<"\n b="<<b;
+    }
+};
+class C:public A,public B{
+    int c;
+    public:
+    void input(){
+        cout<<"Enter value of c";
+        cin>>c;
+        A::input();
+        B::input();
+    }
+    void show(){
+        cout<<"\n c="<<c;
+    }
+};
+int main(){
+    C aa;
+    aa.input();
+    aa.display();
+    aa.print();
+    aa.show();
+    return 0;
+}

cb8aaee0b759cdeebcbdb048432a618c128c1022
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2025-01-07 22:49:52 +0530

    Add files via upload

---
+++ constructoroverloading.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class demo{
+    int a;
+    public:
+    demo()
+    {
+        a=10;
+    }
+    demo(int x)
+    {
+        a=x;
+    }
+    demo(demo &z)
+    {
+        a=z.a;
+    }
+    void putdata()
+    {
+        cout<<"\n a="<<a;
+    }
+};
+int main()
+{
+    demo aa;
+    demo bb(20);
+    demo cc=bb;
+    aa.putdata();
+    bb.putdata();
+    cc.putdata();
+    return 0;
+}

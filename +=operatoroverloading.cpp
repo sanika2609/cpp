90599f615d99510ab28b87b92bbd75e90cbe0786
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2025-01-29 09:51:10 -0800

    Add files via upload

---
+++ +=operatoroverloading.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class demo{
+    int a;
+    public:
+    void getdata(){
+        cout<<"Enter value of a";
+        cin>>a;
+    }
+    void putdata(){
+        cout<<"\n a="<<a;
+    }
+
+void operator  +=(demo bb)
+{
+    a=a+bb.a;
+
+}
+};
+int main(){
+    demo aa,bb;
+    aa.getdata();
+    bb.getdata();
+    aa+=bb;
+    bb.putdata();
+    aa.putdata();
+    return 0;
+}
